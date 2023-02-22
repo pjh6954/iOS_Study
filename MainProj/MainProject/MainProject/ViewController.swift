586bc1356818a29bc7cb5bc38592c935b6acae8f
Dannian <pjh7678@naver.com>
2023-02-22 14:14:00 +0900

    Main Project 생성

---
+++ MainProj/MainProject/MainProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MainProject
+//
+//  Created by JunHo Park on 2023/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MainProj/MainProject/MainProjectUITests/MainProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MainProjectUITestsLaunchTests.swift
+//  MainProjectUITests
+//
+//  Created by JunHo Park on 2023/02/22.
+//
+
+import XCTest
+
+final class MainProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
