187e04a24a7b0394c1985cc07c43ba9ffdb074bc
Dannian <pjh7678@naver.com>
2023-02-27 15:29:12 +0900

    Watch App 예제를 위한 프로젝트 기본 세팅

---
+++ UsingWatchAppWithiOSApp/UsingWatchAppWithiOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsingWatchAppWithiOSApp
+//
+//  Created by JunHo Park on 2023/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UsingWatchAppWithiOSApp/WatchApp Watch App/WatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAppApp.swift
+//  WatchApp Watch App
+//
+//  Created by JunHo Park on 2023/02/27.
+//
+
+import SwiftUI
+
+@main
+struct WatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
