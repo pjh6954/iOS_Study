95da8b49eef060dace3cc31427d19adf965124ff
Dannian <pjh7678@naver.com>
2023-03-07 17:29:19 +0900

    Alamofire Copy project add(분석 및 공부)

---
+++ MainProj/Alamofire-Copy/Alamofire-Copy-iOS-Example/Alamofire-Copy-iOS-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alamofire-Copy-iOS-Example
+//
+//  Created by JunHo Park on 2023/03/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MainProj/Alamofire-Copy/Alamofire-Copy/Alamofire_Copy.h
@@ -0,0 +1,18 @@
+//
+//  Alamofire_Copy.h
+//  Alamofire-Copy
+//
+//  Created by JunHo Park on 2023/03/07.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Alamofire_Copy.
+FOUNDATION_EXPORT double Alamofire_CopyVersionNumber;
+
+//! Project version string for Alamofire_Copy.
+FOUNDATION_EXPORT const unsigned char Alamofire_CopyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Alamofire_Copy/PublicHeader.h>
+
+
