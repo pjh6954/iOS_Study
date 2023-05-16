e4b167a2298046074d6ed1f32225453281f270f6
Junho Park <pjh7678@naver.com>
2023-05-16 15:56:56 +0900

    Widget Ticker 예제 앱을 만들기 위한 프로젝트 세팅

---
+++ MainProj/WidgetTickerExampleApp/WidgetTickerExampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WidgetTickerExampleApp
+//
+//  Created by JunHo Park on 2023/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
